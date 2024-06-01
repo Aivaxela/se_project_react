7b831fd17646d1a0b290a4adf9ae699e7f812699
Aivaxela <rmarcum13@gmail.com>
2024-05-31 20:25:52 -0400

    main created

--- vite-project/src/components/App.jsx
+++ vite-project/src/components/App.jsx
@@ -1,12 +1,14 @@
 import "../blocks/App.css";
 import { currentDate } from "../utils/constants.js";
 import Header from "./Header";
+import Main from "./Main";
 
 function App() {
   return (
     <div className="page">
       <div className="page__content">
         <Header date={currentDate} />
+        <Main />
       </div>
     </div>
   );
---
+++ vite-project/src/components/Main.jsx
@@ -0,0 +1,8 @@
+// import "../blocks/Main.css";
+// import avatarImg from "../assets/avatar.svg";
+
+function Main() {
+  return <header className="header">{console.log("hello from main")}</header>;
+}
+
+export default Main;
