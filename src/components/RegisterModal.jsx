99599f799b6e4bb2315019f58484282c24982579
Riley Marcum <rmarcum13@gmail.com>
2024-09-02 22:19:38 -0400

    remove empty el tags

--- src/components/RegisterModal.jsx
+++ src/components/RegisterModal.jsx
@@ -18,107 +18,105 @@ function RegisterModal({ isOpen, isLoading, handleRegistration }) {
   };
 
   return (
-    <>
-      <ModalWithForm
-        title="Sign up"
-        buttonText={isLoading ? "Registering" : "Next"}
-        altButtonText={"or Log in"}
-        altButtonClick={() => setActiveModal("login")}
-        isOpen={isOpen}
-        onSubmit={handleSubmit}
-        formValid={isValid}
-      >
-        <label htmlFor="email-signup" className="modal__label">
-          Email*
-          <input
-            type="email"
-            className="modal__input"
-            id="email-signup"
-            name="email"
-            placeholder="Email"
-            minLength="4"
-            maxLength="64"
-            required
-            onChange={handleChange}
-            value={values.email || ""}
-          />
-          <span
-            className={`modal__input-error ${
-              errors.email ? "modal__input-error_visible" : ""
-            }`}
-            id="email-error"
-          >
-            {errors.email}
-          </span>
-        </label>
-        <label htmlFor="password-signup" className="modal__label">
-          Password*
-          <input
-            type="password"
-            className="modal__input"
-            id="password-signup"
-            name="password"
-            placeholder="Password"
-            minLength="8"
-            maxLength="64"
-            required
-            onChange={handleChange}
-            value={values.password || ""}
-          />
-          <span
-            className={`modal__input-error ${
-              errors.password ? "modal__input-error_visible" : ""
-            }`}
-            id="password-error"
-          >
-            {errors.password}
-          </span>
-        </label>
-        <label htmlFor="name-signup" className="modal__label">
-          Name*
-          <input
-            type="text"
-            className="modal__input"
-            id="name-signup"
-            name="username"
-            placeholder="Name"
-            minLength="4"
-            maxLength="16"
-            required
-            onChange={handleChange}
-            value={values.username || ""}
-          />
-          <span
-            className={`modal__input-error ${
-              errors.username ? "modal__input-error_visible" : ""
-            }`}
-            id="name-error"
-          >
-            {errors.username}
-          </span>
-        </label>
-        <label htmlFor="avatarUrl" className="modal__label">
-          Avatar URL
-          <input
-            type="url"
-            className="modal__input"
-            id="avatarUrl"
-            name="avatarUrl"
-            placeholder="Avatar Url"
-            onChange={handleChange}
-            value={values.avatarUrl || ""}
-          />
-          <span
-            className={`modal__input-error ${
-              errors.avatarUrl ? "modal__input-error_visible" : ""
-            }`}
-            id="avatarUrl-error"
-          >
-            {errors.avatarUrl}
-          </span>
-        </label>
-      </ModalWithForm>
-    </>
+    <ModalWithForm
+      title="Sign up"
+      buttonText={isLoading ? "Registering" : "Next"}
+      altButtonText={"or Log in"}
+      altButtonClick={() => setActiveModal("login")}
+      isOpen={isOpen}
+      onSubmit={handleSubmit}
+      formValid={isValid}
+    >
+      <label htmlFor="email-signup" className="modal__label">
+        Email*
+        <input
+          type="email"
+          className="modal__input"
+          id="email-signup"
+          name="email"
+          placeholder="Email"
+          minLength="4"
+          maxLength="64"
+          required
+          onChange={handleChange}
+          value={values.email || ""}
+        />
+        <span
+          className={`modal__input-error ${
+            errors.email ? "modal__input-error_visible" : ""
+          }`}
+          id="email-error"
+        >
+          {errors.email}
+        </span>
+      </label>
+      <label htmlFor="password-signup" className="modal__label">
+        Password*
+        <input
+          type="password"
+          className="modal__input"
+          id="password-signup"
+          name="password"
+          placeholder="Password"
+          minLength="8"
+          maxLength="64"
+          required
+          onChange={handleChange}
+          value={values.password || ""}
+        />
+        <span
+          className={`modal__input-error ${
+            errors.password ? "modal__input-error_visible" : ""
+          }`}
+          id="password-error"
+        >
+          {errors.password}
+        </span>
+      </label>
+      <label htmlFor="name-signup" className="modal__label">
+        Name*
+        <input
+          type="text"
+          className="modal__input"
+          id="name-signup"
+          name="username"
+          placeholder="Name"
+          minLength="4"
+          maxLength="16"
+          required
+          onChange={handleChange}
+          value={values.username || ""}
+        />
+        <span
+          className={`modal__input-error ${
+            errors.username ? "modal__input-error_visible" : ""
+          }`}
+          id="name-error"
+        >
+          {errors.username}
+        </span>
+      </label>
+      <label htmlFor="avatarUrl" className="modal__label">
+        Avatar URL
+        <input
+          type="url"
+          className="modal__input"
+          id="avatarUrl"
+          name="avatarUrl"
+          placeholder="Avatar Url"
+          onChange={handleChange}
+          value={values.avatarUrl || ""}
+        />
+        <span
+          className={`modal__input-error ${
+            errors.avatarUrl ? "modal__input-error_visible" : ""
+          }`}
+          id="avatarUrl-error"
+        >
+          {errors.avatarUrl}
+        </span>
+      </label>
+    </ModalWithForm>
   );
 }
 
