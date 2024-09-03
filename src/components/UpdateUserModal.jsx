5804311c0b61c82fdb66de7ff00cd92b372fb38a
Riley Marcum <rmarcum13@gmail.com>
2024-09-02 22:20:05 -0400

    remove empty el tags

--- src/components/UpdateUserModal.jsx
+++ src/components/UpdateUserModal.jsx
@@ -21,59 +21,57 @@ function UpdateUserModal({ isOpen, handleUpdateUser, isLoading }) {
   };
 
   return (
-    <>
-      <ModalWithForm
-        title={"Change profile data"}
-        buttonText={isLoading ? "Saving..." : "Save changes"}
-        isOpen={isOpen}
-        onSubmit={handleSubmit}
-        formValid={isValid}
-      >
-        <label htmlFor="name-update" className="modal__label">
-          Name*
-          <input
-            type="text"
-            className="modal__input"
-            id="name-update"
-            name="username"
-            placeholder="Name"
-            minLength="4"
-            maxLength="64"
-            required
-            onChange={handleChange}
-            value={values.username || ""}
-          />
-          <span
-            className={`modal__input-error ${
-              errors.username ? "modal__input-error_visible" : ""
-            }`}
-            id="name-error"
-          >
-            {errors.username}
-          </span>
-        </label>
-        <label htmlFor="avatarUrl-update" className="modal__label">
-          Avatar URL
-          <input
-            type="url"
-            className="modal__input"
-            id="avatarUrl-update"
-            name="avatarUrl"
-            placeholder="Avatar Url"
-            onChange={handleChange}
-            value={values.avatarUrl || ""}
-          />
-          <span
-            className={`modal__input-error ${
-              errors.avatarUrl ? "modal__input-error_visible" : ""
-            }`}
-            id="avatarUrl-error"
-          >
-            {errors.avatarUrl}
-          </span>
-        </label>
-      </ModalWithForm>
-    </>
+    <ModalWithForm
+      title={"Change profile data"}
+      buttonText={isLoading ? "Saving..." : "Save changes"}
+      isOpen={isOpen}
+      onSubmit={handleSubmit}
+      formValid={isValid}
+    >
+      <label htmlFor="name-update" className="modal__label">
+        Name*
+        <input
+          type="text"
+          className="modal__input"
+          id="name-update"
+          name="username"
+          placeholder="Name"
+          minLength="4"
+          maxLength="64"
+          required
+          onChange={handleChange}
+          value={values.username || ""}
+        />
+        <span
+          className={`modal__input-error ${
+            errors.username ? "modal__input-error_visible" : ""
+          }`}
+          id="name-error"
+        >
+          {errors.username}
+        </span>
+      </label>
+      <label htmlFor="avatarUrl-update" className="modal__label">
+        Avatar URL
+        <input
+          type="url"
+          className="modal__input"
+          id="avatarUrl-update"
+          name="avatarUrl"
+          placeholder="Avatar Url"
+          onChange={handleChange}
+          value={values.avatarUrl || ""}
+        />
+        <span
+          className={`modal__input-error ${
+            errors.avatarUrl ? "modal__input-error_visible" : ""
+          }`}
+          id="avatarUrl-error"
+        >
+          {errors.avatarUrl}
+        </span>
+      </label>
+    </ModalWithForm>
   );
 }
 
